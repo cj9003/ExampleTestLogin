7b7452d1b21bb1db7362482a06755c93635e65c7
cj9003 <carlos.javier9003@gmail.com>
2025-05-14 15:19:17 -0400

    a

--- tests/steps_defs/test_login_step.py
+++ tests/steps_defs/test_login_step.py
@@ -8,18 +8,18 @@ URL = "https://practicetestautomation.com/practice-test-login/"
 
 scenarios("login.feature")
 
-    
-@given('Open the login page', target_fixture="browser") 
+
+@given('Open the login page', target_fixture="browser")
 def browser():
     driver = webdriver.Chrome()  # Initialize the WebDriver (e.g., Chrome)
     driver.get(URL)
-    # Wait for the page to load 
+    # Wait for the page to load
     driver.implicitly_wait(10)
     yield driver
     driver.quit()  # Ensure the browser is closed after the test
-    
 
-#when steps
+
+# when steps
 
 @when(parsers.parse('I enter {user} and {passw}'))
 def valid_credentials(browser, user, passw):
@@ -28,15 +28,13 @@ def valid_credentials(browser, user, passw):
     browser.find_element(By.ID, "submit").click()
     browser.implicitly_wait(10)
 
-#then steps
+# then steps
+
 
 @then('I should be redirected to the dashboard')
 def logged(browser):
+    browser.implicitly_wait(10)
     if browser.current_url == "https://practicetestautomation.com/practice-test-login/":
-       assert browser.find_element(By.ID, "error").is_displayed()
+        assert browser.find_element(By.ID, "error").is_displayed()
     else:
         assert browser.current_url == "https://practicetestautomation.com/logged-in-successfully/", "wrong credentials"
-
-
-
-    
